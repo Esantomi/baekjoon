0c352968ef5004ead9247c27d451d674f32514fc
DHaein <pilinnasa@gmail.com>
2022-06-27 17:13:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,14 +1,7 @@
 A, B = map(int, input().split())
-C = int(input())  # 소요 시간(분)
+C = int(input())
 
-# 소요 시간 분배
-A += C // 60
-B += C % 60 
-
-if B >= 60:
-  A += 1
-  B -= 60
-if A > 23:
-  A -= 24
+A = (A + (B+C)//60) % 24
+B = (B+C)%60
 
 print(A, B)
