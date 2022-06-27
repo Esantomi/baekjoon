255a6bc3f45d3efdf83d615eaaa344ef04a43bfe
DHaein <pilinnasa@gmail.com>
2022-06-27 17:44:45 +0900

    [Bronze V] Title: A+B - 3, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,10 @@
+# 테스트 케이스 개수
+T = int(input())
+
+for _ in range(T):
+  A, B = map(int, input().split())
+  print(A + B)
+    
+# for _ in iterable:
+  # iterable 자료 요소 미사용
+  # for i in range(T)도 가능
