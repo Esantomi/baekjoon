59d0295b41dd057d3efb449ccfe7faff1a2264ea
DHaein <pilinnasa@gmail.com>
2022-07-07 00:12:04 +0900

    [Bronze IV] Title: 세수정렬, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -0,0 +1,5 @@
+arr = sorted(map(int, input().split()))
+print(f'{arr[0]} {arr[1]} {arr[2]}')
+
+# print("{} {} {}".format(*sorted(map(int, input().split()))))
+  # *는 list의 각 요소를 함수의 argument로 넣어 줌
