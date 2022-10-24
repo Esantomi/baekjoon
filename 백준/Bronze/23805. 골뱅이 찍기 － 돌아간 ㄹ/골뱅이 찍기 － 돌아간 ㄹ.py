46ef40b87ef308c965d4f17621ce7efeed704850
DHaein <pilinnasa@gmail.com>
2022-10-25 08:57:52 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.py
@@ -0,0 +1,8 @@
+N= int(input())
+
+for _ in range(N):
+    print('@'*3*N + ' '*N + '@'*N)
+for _ in range(3*N):
+    print('@'*N + ' '*N + '@'*N + ' '*N + '@'*N)
+for _ in range(N):
+    print('@'*N + ' '*N + '@'*3*N)
